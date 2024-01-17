d78f2a50b70ba585ecaf5ba9c5b2ee6a2081f67f
mahdi <mahdi.mcheik@hotmail.fr>
2024-01-17 19:00:10 +0100

    user back end done

--- js/backend/src/router.js
+++ js/backend/src/router.js
@@ -1,23 +1,28 @@
 const express = require("express");
+const userControllers = require("./controllers/userController");
 
 const router = express.Router();
 
-/* ************************************************************************* */
-// Define Your API Routes Here
-/* ************************************************************************* */
-
-// Import itemControllers module for handling item-related operations
-const itemControllers = require("./controllers/itemControllers");
 
 // Route to get a list of items
-router.get("/items", itemControllers.browse);
-
-// Route to get a specific item by ID
-router.get("/items/:id", itemControllers.read);
+router.get("/users", userControllers.browse);
 
 // Route to add a new item
-router.post("/items", itemControllers.add);
+router.post("/users", userControllers.add);
 
+// Route to check existed user
+router.post("/user", userControllers.check);
+
+// Route to edit existed user
+router.put("/user", userControllers.edit);
+
+// Route to delete existed users
+router.delete("/user", userControllers.destroy);
+// activateAccount
+// re-send activation code
+router.patch("/npassword", userControllers.updatePassword);
+// resend code  used to reset password
+router.post("/reset", userControllers.updatePassword);
 /* ************************************************************************* */
 
 module.exports = router;
