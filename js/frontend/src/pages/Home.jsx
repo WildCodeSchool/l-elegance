b7b52caa21deb27c234cc44406a3f327ea513c8e
Nassitch <Nassitch@hotmail.fr>
2024-01-18 17:00:03 +0100

    Finished 'Pop up' component.

--- js/frontend/src/pages/Home.jsx
+++ js/frontend/src/pages/Home.jsx
@@ -1,3 +1,10 @@
-export default function Home(){
-    return <h1>Home</h1>
+import Modale from "../components/Modale pop up/Modale";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Modale />
+    </div>
+  );
 }
