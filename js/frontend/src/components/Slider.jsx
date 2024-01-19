8565641118dc986681a8da0cd00356450fc5e57b
mahdi <mahdi.mcheik@hotmail.fr>
2024-01-19 02:00:07 +0100

    commit after pull

---
+++ js/frontend/src/components/Slider.jsx
@@ -0,0 +1,101 @@
+import "../../style/slider.css";
+
+const product = [
+  {
+    id: 1,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+  {
+    id: 2,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+  {
+    id: 3,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+  {
+    id: 4,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+  {
+    id: 5,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+  {
+    id: 6,
+    title: "Curl Expressioncurls reviver leav-in.",
+    image:
+      "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+  },
+//   {
+//     id: 7,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+//   {
+//     id: 8,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+//   {
+//     id: 9,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+//   {
+//     id: 10,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+//   {
+//     id: 11,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+//   {
+//     id: 12,
+//     title: "Curl Expressioncurls reviver leav-in.",
+//     image:
+//       "https://img01.ztat.net/article/spp-media-p1/cc63c6ac71484805ad197d9442280000/4fe79f223d2747a7a98a7cc497cf2c53.jpg?imwidth=1800&filter=packshot",
+//   },
+];
+
+function Slider() {
+  return (
+    <div className="suggestion-make-up">
+      <div className="statement">
+        <h3 className="sub-title">Découvrir</h3>
+        <h2 className="title">L'Oréal à de l'élégence.</h2>
+        <p className="description">
+          Découvrez les réalisations, les engagements et les aspirations qui
+          inspirent L'Oréal depuis plus de 100 ans.
+        </p>
+      </div>
+      <div className="slide-make-up">
+        {product.map((product) => (
+          <div key={product.id} className="card-make-up">
+            <img src={product.image} />
+            <p>{product.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Slider;
--- js/frontend/src/pages/Home.jsx
+++ js/frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Footer from "../components/Footer";
 import Nav from "../components/Nav";
 
 function Home() {
+<<<<<<< HEAD
     const navigate = useNavigate();
     return (
         <>
@@ -63,6 +64,57 @@ function Home() {
             <Footer />
         </>
     );
+=======
+  return (
+    <>
+      <Nav />
+      <Bandeau />
+      <div className="text-resultat">
+        <div className="container">
+          <h2>LE CONCEPT</h2>
+          <h3>Confiez-vous, nous vous guiderons au mieux</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            aliquam in lectus in consectetur. Suspendisse justo quam, eleifend
+            quis suscipit id, porttitor at nunc. Phasellus sit amet neque
+            bibendum, mollis nisi ac, tincidunt lorem. Cras lobortis tortor quis
+            nisl semper, id interdum nibh tincidunt..
+          </p>
+          <button type="button" className="btn-more-round">
+            {" "}
+            Faire le diagnostic
+          </button>
+        </div>
+      </div>
+      <div className="container container-presentation">
+        <div className="container-text-presentation-home">
+          <h2>Confiez-vous, nous vous guiderons au mieux</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            aliquam in lectus in consectetur. Suspendisse justo quam, eleifend
+            quis suscipit id, porttitor at nunc. Phasellus sit amet neque
+            bibendum, mollis nisi ac, tincidunt lorem. Cras lobortis tortor quis
+            nisl semper, id interdum nibh tincidunt. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit. Vivamus aliquam in lectus in
+            consectetur. Suspendisse justo quam, eleifend quis suscipit id,
+            porttitor at nunc. Phasellus sit amet neque bibendum, mollis nisi
+            ac, tincidunt lorem. Cras lobortis tortor quis nisl semper, id
+            interdum nibh tincidunt..
+          </p>
+          <button type="button" className="btn-more-round">
+            {" "}
+            Faire le diagnostic
+          </button>
+        </div>
+
+        <div className="container-img-presentation">
+          <img className="img-fluid" src="../src/assets/img-basic.png" alt="" />
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+>>>>>>> 8a911f75176c3fb2934c3a47f4f2707769823f19
 }
 
 export default Home;
